36760171cbc72993a90e776a1fd07d33411c0406
ditsuke <ditsuke@protonmail.com>
2024-02-27 11:47:51 +0530

    chore: Move cmake to nativeBuildInputs for devShell

--- .devops/nix/package.nix
+++ .devops/nix/package.nix
@@ -215,9 +215,8 @@ effectiveStdenv.mkDerivation (finalAttrs: {
     shell = mkShell {
       name = "shell-${finalAttrs.finalPackage.name}";
       description = "contains numpy and sentencepiece";
-      buildInputs = [
+      nativeBuildInputs = [
         cmake
-        gcc
       ];
       inputsFrom = [ finalAttrs.finalPackage ];
     };
